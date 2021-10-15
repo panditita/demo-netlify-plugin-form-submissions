5a103bc2c270362b4e31c342c117e92296d4db0b
Etzali <etzali@passion4social.com>
2021-10-15 14:57:56 +0100

    Refactors creation of data.json and mapbox markers

--- fetchSubmissions.js
+++ fetchSubmissions.js
@@ -1,20 +1,11 @@
 "use strict";
 import fetch from "node-fetch";
 import fs from "fs";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 const oauth_token = process.env.NETLIFY_AUTH_TOKEN;
 
-function createCoordinates(responsesArray) {
-  let coordinates = [];
-  for (let i of responsesArray) {
-    let pairedCoordinates = [];
-    pairedCoordinates.push(parseFloat(i.lng));
-    pairedCoordinates.push(parseFloat(i.lat));
-    coordinates.push(pairedCoordinates);
-  }
-  return coordinates;
-}
+
 
 // get form submissions from netlify and write to json file
 fetch(
@@ -23,16 +14,42 @@ fetch(
   .then((response) => response.text())
   .then((result) => {
     let results = JSON.parse(result);
-    let responsesArray = [];
-    
-    for (let res of results) {
-      responsesArray.push(res.data);
-    }
-    let coordinates = {
-      coordinates: createCoordinates(responsesArray),
-    };
-    
-    const jsonString = JSON.stringify(coordinates);
+    let submission = {}
+    let submissions = []
+    results.forEach((res) => {
+
+      submission = {
+        'id': res.id,
+        'archive-name': res.data['archive-name'],
+        'archive-description': res.data['archive-description'],
+        'archive-time': res.data['archive-time'],
+        'archive-country': res.data['archive-country'],
+        coordinates: {
+          lng: res.data.lng,
+          lat: res.data.lat
+        },
+        'archive-link': res.data['archive-link'],
+        'archive-tags': res.data['archive-tags'],
+        'archive-type-materials': {
+          audio: res.data.audio,
+          film: res.data.film,
+          ephemer: res.data.ephemer,
+          images: res.data.images,
+          'official-records': res.data['official-records'],
+          'fine-art': res.data['fine-art'],
+          correspondence: res.data.correspondence,
+          'published-reports': res.data['published-reports'],
+          maps: res.data.maps,
+          unknown: res.data.unknown,
+        },
+      }
+      submissions.push(submission)
+    });
+
+    console.log('submission',submissions);
+   
+    const jsonString = JSON.stringify(submissions);
+
     fs.writeFile("data.json", jsonString, (err) => {
       if (err) {
         console.log("Error writing file", err);
--- src/scripts/createMap.js
+++ src/scripts/createMap.js
@@ -3,22 +3,27 @@ const mapboxgl = require("mapbox-gl");
 fetch("data.json")
   .then((response) => response.text())
   .then((result) => {
-    console.log('result',result);
     let results = JSON.parse(result);
+
+    function createCoordinates(result) {
+        let pairedCoordinates = [];
+        pairedCoordinates.push(parseFloat(result.coordinates.lng));
+        pairedCoordinates.push(parseFloat(result.coordinates.lat));
+      return pairedCoordinates;
+    }
+
     mapboxgl.accessToken =
       "pk.eyJ1IjoibXVsdGl0dWRlcyIsImEiOiJja3VtbXRyM3cwZzQ3MnZwMTdoenRheXUxIn0.mTaqrjMcME57vZlFxGIRJQ";
     const map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/multitudes/ckuplhk1b4gg217o7zpc74lhi",
-      center: [0,56],
-      zoom: 4
+      center: [0, 56],
+      zoom: 4,
     });
-
-    results.coordinates.forEach((coordinate)=>{
-      new mapboxgl.Marker().setLngLat(coordinate).addTo(map);
+    
+    results.forEach((result) => {
+      new mapboxgl.Marker().setLngLat(createCoordinates(result)).addTo(map);
     });
-
   })
+
   .catch((error) => console.log("error", error));
-
-
